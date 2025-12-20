8d9a4060f0476ec444dd285fe32c96977d12930a
putrifaradilah <putrifaradilah491@gmail.com>
2025-12-20 07:22:03 +0700

    Add files via upload

---
+++ Pertemuan 7/TipeDataFunction.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int tambah(int a, int b){
+    return a + b;
+}
+
+int main(){
+    cout << "Hasil penjumlahan adalah " << tambah(5,2);
+}
---
+++ Pertemuan 7/VoidFunction.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+void perkenalkan(){
+    cout << "Hi, nama saya adalah Putri Faradilah" << endl;
+    cout << "Saya berasal dari Program Studi Ilmu Komputer" << endl;
+    cout << "Saya dari kelas B" << endl;
+}
+
+int main(){
+    perkenalkan();
+
+    return 0;
+}
